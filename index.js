e9bcf2154e5e1a812730c2bff426f1e947d31ae7
Juan Manuel Bentancor <juanmanbe222@gmail.com>
2024-11-10 17:53:39 -0300

    React App Ready

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // const express = require('express');
+import path from 'path'
+import { fileURLToPath } from 'url';
+// Definir __dirname para ES6 modules
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 import express from 'express'
 import dotenv from 'dotenv';
 import authRouter from './routes/auth.js'; //-authRouter es un alias que le elegimos a la importacion, podemos inventar cualquiera
@@ -29,6 +33,10 @@ app.use(express.json());
 //------------ Rutas ------------------
 app.use('/api/auth', authRouter); //-- auth, crear , login, renovacion
 app.use('/api/events', eventRouter); //-- CRUD eventos
+
+app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+});
 
 
 //- Escuchar peticiones, nota: no usar el mismo puerto que el front o alguno que este usado
